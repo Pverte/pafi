83738e0739859d03ff159cc2085e8bc8e3063332
Pverte <63103558+Pverte@users.noreply.github.com>
2022-02-15 21:29:01 +0100

    Now can make track links

--- main.py
+++ main.py
@@ -22,22 +22,30 @@ intents = discord.Intents(
     messages=True,
 )
 
-def getinformations(ctx):
+def getinfodeezer(ctx):
     """
     Input : Deezer Album link
     Output : artist, artistimage, link, coverimage, albumname"""
-    global artist,  artistimage, link, coverimage, albumname
+    global artist,  artistimage, link, coverimage, albumname, title, bpm
     ctx = str(ctx)
-    apilink = ctx[:8] + "api" + ctx[11:]
+    apilink = "https://api.deezer.com/" + ctx[-16:]
     print(apilink)
     informations = requests.get(apilink)
     text = informations.json()
-    
-    artist = text["artist"]["name"]
-    artistimage = text["artist"]["picture_medium"]
-    link= text["link"]
-    coverimage = text["cover_medium"]
-    albumname = text["title"]
+    if "album" in apilink:
+        artist = text["artist"]["name"]
+        artistimage = text["artist"]["picture_medium"]
+        link= text["link"]
+        coverimage = text["cover_medium"]
+        albumname = text["title"]
+    if "track" in apilink:
+        artist = text["artist"]["name"]
+        artistimage = text["artist"]["picture_medium"]
+        link= text["link"]
+        coverimage = text["album"]["cover_medium"]
+        title = text["title"]
+        albumname = text["album"]["title"]
+        bpm = text["bpm"]
 
 def covercolor(coverurl):
     response = requests.get(coverurl)
@@ -70,34 +78,72 @@ async def help(ctx):
 
 @bot.slash_command(guild_ids=[694443902526029854], name="deezer", description="Let's find some Deezer Music.")
 async def deezer(ctx, link):
-    #if "deezer.com" in ctx:
-        print(link)
-        getinformations(link)
-        print(coverimage)
-        e = discord.Embed(
-            title = "Informations :",
-            color = covercolor(coverimage),
+    if "deezer.com" in link:
+        if "album" in link:
+            print(link)
+            getinfodeezer(link)
+            print(coverimage)
+            e = discord.Embed(
+                color = covercolor(coverimage),
+                )
+            e.add_field(
+                name="Album name",
+                value=albumname,
+                inline= False
             )
-        e.add_field(
-            name="Album name",
-            value=albumname,
-            inline= False
-        )
-        e.set_thumbnail(url=coverimage)
-        e.add_field(
-            name="Listen it here :",
-            value=f"[Deezer]({link})"
-        )
-        e.add_field(
-            name="\u200B",
-            value="Bot by [Pverte](https://pverte.me)",
-            inline=False
-        )
-
-        e.set_author(name= f"By {artist}", icon_url=artistimage)
-        await ctx.respond(embed=e)
-    #else:
-    #    await ctx.respond("I don't see this as a deezer link, please restart, it have to look like htpps://deezer.com/album/302127")
+            e.set_thumbnail(url=coverimage)
+            e.add_field(
+                name="Listen it here :",
+                value=f"[Deezer]({link})"
+            )
+            e.add_field(
+                name="\u200B",
+                value="Bot by [Pverte](https://pverte.me)",
+                inline=False
+            )
+            e.set_author(name= f"By {artist}", icon_url=artistimage)
+            await ctx.respond(embed=e)
+        elif "track" in link:
+            print(link)
+            getinfodeezer(link)
+            print(coverimage)
+            e = discord.Embed(
+                color = covercolor(coverimage),
+                )
+            e.add_field(
+                name="Title",
+                value=title,
+                inline=False
+            )
+            e.add_field(
+                name="Album name",
+                value=albumname,
+                inline= True
+            )
+            if bpm == 0:
+                bpm1 = "Unavailable"
+            else :
+                bpm1 = bpm
+            e.add_field(
+                name="BPM",
+                value=bpm1,
+                inline=True
+            )
+            e.set_thumbnail(url=coverimage)
+            e.add_field(
+                name="Listen it here :",
+                value=f"[Deezer]({link})",
+                inline=False
+            )
+            e.add_field(
+                name="\u200B",
+                value="Bot by [Pverte](https://pverte.me)",
+                inline=False
+            )
+            e.set_author(name= f"By {artist}", icon_url=artistimage)
+            await ctx.respond(embed=e)
+    else:
+        await ctx.respond("I don't see this as a deezer link, please restart, it have to look like htpps://deezer.com/album/302127")
 @bot.slash_command(name='greet', description='Greet someone!', guild_ids=[694443902526029854])
 async def greet(ctx, name=''):
     await ctx.respond(f'Hello {name}!')
