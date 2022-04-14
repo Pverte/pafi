efeb9d4cbde2142cc2e649e4926b31e87c8eaf16
Pverte <63103558+Pverte@users.noreply.github.com>
2022-04-14 23:12:26 +0900

    Updated the way it get informations + give facts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Imports of the necessary libraries requests for web resquest, discord for the discord bot, json for formatting the informations received by the api
+import traceback
 import requests
 import discord
 from discord.ext import commands
@@ -12,6 +13,9 @@ import binascii
 from PIL import Image
 import start
 import time
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 
 NUM_CLUSTERS = 5
 
@@ -25,8 +29,7 @@ intents = discord.Intents(
 def deezerinfo(ctx):
     """
     Input : Deezer link
-    Output : artist, artistimage, link, coverimage, albumname"""
-    global artist,  artistimage, coverimage, albumname, title, bpm
+    Output : the artist name, the title and what type is it"""
     if "deezer.page.link" in ctx:
         ctx = requests.get(ctx).url
     if "?" in ctx:
@@ -39,24 +42,72 @@ def deezerinfo(ctx):
     deezerinfo.link= text["link"]
     if "album" in apilink:
         artist = text["artist"]["name"]
-        artistimage = text["artist"]["picture_medium"]
-        deezerinfo.date = text["release_date"]
-        coverimage = text["cover_medium"]
-        albumname = text["title"]
+        title = text["title"]
+        what = "album"
     if "track" in apilink:
         artist = text["artist"]["name"]
-        artistimage = text["artist"]["picture_medium"]
-        deezerinfo.date = text["release_date"]
-        coverimage = text["album"]["cover_medium"]
         title = text["title"]
-        albumname = text["album"]["title"]
-        bpm = text["bpm"]
+        what = "track"
     if "artist" in apilink:
         artist=text["name"]
-        artistimage=text["picture_medium"]
-        coverimage=text["picture_big"]
-        deezerinfo.nb_album=text["nb_album"]
-        deezerinfo.nb_fan=text["nb_fan"]
+        what = "artist"
+        title = text["name"]
+    return artist, what, title
+
+def getinfosfromspotifyapi(artist, what, title):
+    client_credentials_manager = SpotifyClientCredentials(client_id=start.client_id, client_secret=start.client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    if what == "track":
+        results = sp.search(q='track:' + title + ' artist:' + artist, type=what)
+        #returns the name of the name of the track, the name of the artist, the name of the album, the link to the album cover, the link to the track, the link to the artist
+        infos = {
+            "name": results["tracks"]["items"][0]["name"], 
+            "artist": results["tracks"]["items"][0]["artists"][0]["name"], 
+            "album": results["tracks"]["items"][0]["album"]["name"], 
+            "cover": results["tracks"]["items"][0]["album"]["images"][0]["url"], 
+            "link": results["tracks"]["items"][0]["external_urls"]["spotify"], 
+            "artistlink": results["tracks"]["items"][0]["artists"][0]["external_urls"]["spotify"], 
+            "type": results["tracks"]["items"][0]["type"],
+            "artistcover": sp.artist(results["tracks"]["items"][0]["artists"][0]["id"])["images"][0]["url"],
+            "date": results["tracks"]["items"][0]["album"]["release_date"]
+        }
+        return infos
+    if what == "album":
+        results = sp.search(q='album:' + title + ' artist:' + artist, type="album")
+        infos = {
+            "name": results["albums"]["items"][0]["name"], 
+            "artist": results["albums"]["items"][0]["artists"][0]["name"], 
+            "cover": results["albums"]["items"][0]["images"][0]["url"], 
+            "link": results["albums"]["items"][0]["external_urls"]["spotify"], 
+            "artistlink": results["albums"]["items"][0]["artists"][0]["external_urls"]["spotify"],
+            "type": results["albums"]["items"][0]["type"],
+            "artistcover": sp.artist(results["albums"]["items"][0]["artists"][0]["id"])["images"][0]["url"],
+            "date": results["albums"]["items"][0]["release_date"]
+        }
+        return infos
+    if what == "artist":
+        results = sp.search(q='artist:' + artist, type="artist")
+        print(results)
+        #returns the name of the artist, the link to the artist's image, the link to the artist's page on spotify,  the number of followers of the artist
+        infos = {
+            "name": results["artists"]["items"][0]["name"], 
+            "cover": results["artists"]["items"][0]["images"][0]["url"], 
+            "link": results["artists"]["items"][0]["external_urls"]["spotify"], 
+            "followers": results["artists"]["items"][0]["followers"]["total"],
+            "type": results["artists"]["items"][0]["type"],
+            "artistcover": results["artists"]["items"][0]["images"][0]["url"]
+        }
+        return infos
+
+def waiting(ctx):
+    """
+    The bot give a fact about music to the utilisator while he is looking for the informations about the music
+    """
+    e = discord.Embed(
+        title="While I'm looking for the informations about the music, here a fact about music : ",
+        description=start.facts[np.random.randint(0, len(start.facts))],
+        color=0x00ff00)
+
 
 def covercolor(coverurl):
     response = requests.get(coverurl)
@@ -77,8 +128,8 @@ def covercolor(coverurl):
     print(readableHex)
     return readableHex
 
+
 bot = discord.Bot(activity=discord.Game(name='Finding your music ! Find commands with /help'), intents=intents)
-#discord.ext.commands.Bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -101,115 +152,98 @@ async def on_application_command_error(ctx, error):
 
 @bot.slash_command(guild_ids=[694443902526029854], name="deezer", description="Let's find some Deezer Music !")
 async def deezer(ctx, link):
-    await ctx.defer()
-    deezerinfo(link)
-    print(deezerinfo.link)
+    e = discord.Embed(
+        title="While I'm looking for the informations about the music, here a fact about music : ",
+        description=start.facts[np.random.randint(0, len(start.facts))],
+        color=0x00ff00)
+    await ctx.respond(content="I'm looking for the informations about the music, please wait", embed=e)
+    print(deezerinfo(link))
+    infos = getinfosfromspotifyapi(*(deezerinfo(link)))
     if "album" in deezerinfo.link:
         print(link)
-        print(coverimage)
+        print(infos["cover"])
         e = discord.Embed(
-            color = covercolor(coverimage),
+            color = covercolor(infos["cover"]),
             )
         e.add_field(
             name="Album name",
-            value=albumname,
+            value=infos["name"],
             inline= False
         )
-        e.set_thumbnail(url=coverimage)
+        e.set_thumbnail(url=infos["cover"])
         e.add_field(
             name="Listen it here :",
-            value=f"[Deezer]({deezerinfo.link})"
+            value=f"[Deezer]({deezerinfo.link}) \n [Spotify]({infos['link']})"
         )
         e.add_field(
             name="Release date",
-            value =deezerinfo.date
+            value =infos["date"]
         )
         e.add_field(
             name="\u200B",
             value="Bot by [Pverte](https://pverte.me)",
             inline=False
         )
-        e.set_author(name= f"By {artist}", icon_url=artistimage)
-        await ctx.respond(embed=e)
+        e.set_author(name="By " + infos["artist"], icon_url=(infos["artistcover"]))
     elif "track" in deezerinfo.link:
-        print(coverimage)
+        print(infos["cover"])
         e = discord.Embed(
-            color = covercolor(coverimage),
+            color = covercolor(infos["cover"]),
             )
         e.add_field(
             name="Title",
-            value=title,
+            value=infos["name"],
             inline=False
         )
         e.add_field(
             name="Album name",
-            value=albumname,
+            value=infos["album"],
             inline= True
-        )
-        if bpm == 0:
-            bpm1 = "Unavailable"
-        else :
-            bpm1 = bpm
-        e.add_field(
-            name="BPM",
-            value=bpm1,
-            inline=True
         )
         e.add_field(
             name="Release date",
-            value =deezerinfo.date,
+            value =infos["date"],
             inline=True
         )
-        e.set_thumbnail(url=coverimage)
+        e.set_thumbnail(url=infos["cover"])
         e.add_field(
             name="Listen it here :",
-            value=f"[Deezer]({deezerinfo.link})",
-            inline=False
+            value=f"[Deezer]({deezerinfo.link}) \n [Spotify]({infos['link']})"
         )
         e.add_field(
             name="\u200B",
             value="Bot by [Pverte](https://pverte.me)",
             inline=False
         )
-        e.set_author(name= f"By {artist}", icon_url=artistimage)
-        await ctx.respond(embed=e)
+        e.set_author(name= f"By " + infos["artist"], icon_url=(infos["artistcover"]))
     elif "artist" in deezerinfo.link:
-        print(coverimage)
+        print(infos["cover"])
         e = discord.Embed(
-            color = covercolor(coverimage),
+            color = covercolor(infos["cover"]),
             )
         e.add_field(
             name="Artist",
-            value=artist,
+            value=infos["name"],
             inline=False
         )
-        print(artist)
         e.add_field(
-            name="Number of fans on Deezer",
-            value=deezerinfo.nb_fan,
+            name="Number of fans on Spotify",
+            value=infos["followers"],
             inline= True
         )
-        print(deezerinfo.nb_fan)
-        e.add_field(
-            name="Numbers of albums",
-            value=deezerinfo.nb_album,
-            inline=True
-        )
-        print(deezerinfo.nb_album)
-        e.set_thumbnail(url=coverimage)
+        e.set_thumbnail(url=infos["cover"])
         e.add_field(
             name="Listen it here :",
-            value=f"[Deezer]({deezerinfo.link})",
-            inline=False
+            value=f"[Deezer]({deezerinfo.link}) \n [Spotify]({infos['link']})"
         )
-        print(deezerinfo.link)
         e.add_field(
             name="\u200B",
             value="Bot by [Pverte](https://pverte.me)",
             inline=False
         )
-        e.set_author(name=artist, icon_url=artistimage)
-        await ctx.respond(embed=e)
+        e.set_author(name=infos["name"], icon_url=(infos["artistcover"]))
+    time.sleep(1)
+    await ctx.edit(content="", embed=e)
 
 @bot.slash_command(name='greet', description='Greet someone!', guild_ids=[694443902526029854])
 async def greet(ctx, name=''):
@@ -237,5 +271,6 @@ async def help(ctx):
         inline=False
     )
     e.set_author(name=f"By {pverte}", icon_url=pverte.avatar)
+    time.sleep(4)
     await ctx.respond(embed=e)
 bot.run(start.token)
